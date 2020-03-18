bea1654abc9c7d782ecc60e061957b5d15dba72b
akotaso <oimo.moimo@gmail.com>
2020-03-18 13:13:22 +0900

    devise設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,7 +4,7 @@ class ItemsController < ApplicationController
     @item = Item.new
     @comments = Comment.all
     @comment = Comment.new
-    
+    redirect_to "/users/sign_up" unless user_signed_in?
   end
 
   def new
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,controllers: {    registrations: 'users/registrations',
+                                      sessions: 'users/sessions' }
 
   resources :index, only: :index
   resources :comments, only: [:index, :create]
