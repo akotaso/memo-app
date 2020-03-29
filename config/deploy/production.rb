1bbf3d0956d7c098bbb5f25d791e5e0ab2611953
akotaso <oimo.moimo@gmail.com>
2020-03-30 08:04:29 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '13.112.145.233', user: 'akotaso', roles: %w{app db web}
+server '13.112.145.233', user: 'ec2-user', roles: %w{app db web}
 
 set :rails_env, :production
 set :unicorn_rack_env, "production"
