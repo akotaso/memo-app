644dda5de7824f60fd3ea03cab3e7b79aa2ef559
akotaso <oimo.moimo@gmail.com>
2020-03-30 06:57:49 +0900

    capfile更新

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,17 +8,15 @@ set :application, 'EasyTag'
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:akotaso/EasyTag.git'
 
-set :deploy_to, '/var/www/EasyTag'
-
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
-set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
+set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/aws_git_rsa'] 
+                  keys: ['~/.ssh/portfolio.pem']
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
