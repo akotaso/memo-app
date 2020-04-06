911f0315c900ad23996a5d9decf67471792f02cb
akotaso <oimo.moimo@gmail.com>
2020-04-06 23:58:50 +0900

    インクリメンタルサーチ３

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,17 @@
 $(function(){
   $("#group_form_member-search").on("keyup",function(){
     let input= $("#group_form_member-search").val();
-    console.log(input);
+    $.ajax({
+      type: "GET",
+      url: "/users",
+      data: { keyword: input},
+      dataType: "json"
+    })
+      .done(function(users){
+        console.log("成功です");
+      })
+      .fail(function(){
+        console.log("失敗です");
+      });
   });
 });
