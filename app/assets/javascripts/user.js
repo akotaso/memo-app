f51565e6770de86b7ddc34848a59d63c2a617864
akotaso <oimo.moimo@gmail.com>
2020-04-08 22:44:42 +0900

    インクリメンタルサーチ４

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -3,7 +3,7 @@ $(function(){
     var html= `
       <div class="group_form_member-incri clearfix">
         <p class="group_form_member-incriName">${user.name}</p>
-        <div class="addUserButton" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+        <div class="addUserButton chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
       </div>
     `;
     $("#group_form_member-search-result").append(html);
@@ -16,6 +16,20 @@ $(function(){
       </div>
     `;
     $("#group_form_member-search-result").append(html);
+  }
+
+  function addDeleteUser(name,id) {
+    var html = `
+    <div class="chat-group-user clearfix" id="${id}">
+      <p class="chat-group-user__name">${name}</p>
+      <div class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn" data-user-id="${id}" data-user-name="${name}">削除</div>
+    </div>`;
+    $(".js-add-user").append(html);
+  }
+
+  function addMember(userId) {
+    var html = `<input value="${userId}" name="group[user_ids][]" type="hidden" id="group_user_ids_${userId}" />`;
+    $(`#${userId}`).append(html);
   }
 
   $("#group_form_member-search").on("keyup",function(){
@@ -41,5 +55,20 @@ $(function(){
       .fail(function(){
         alert("通信エラーです。ユーザーが表示できません");
       });
+  });
+
+  $(document).on('click', '.chat-group-user__btn--add', function(){
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this)
+      .parent()
+      .remove();
+    addDeleteUser(userName,userId);
+    addMember(userId);
+  });
+  $(document).on('click','.chat-group-user__btn--remove', function(){
+    $(this)
+      .parent()
+      .remove();
   });
 });
